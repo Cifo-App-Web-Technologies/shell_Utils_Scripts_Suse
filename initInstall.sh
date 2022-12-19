2292a435fa4523afd19d38cd7c121dd9f9bff6f7
alexpicjava <alex.pic.java@gmail.com>
2022-12-19 21:59:43 +0000

    [FIX]Refactoring initInstall.sh

--- initInstall.sh
+++ initInstall.sh
@@ -14,8 +14,8 @@ git config --global user.mail "alex.pic.java@gmail.com"
 #Create a local Repo and clone all necessary information to continue with the installation.
 # git clone https://oauth-key-goes-here@github.com/username/repo.git
 # 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
-# git clone https://github.com/Cifo-App-Web-Technologies/shell_Utils_Scripts.git /home/$USER/shell_Utils_Scripts
-git clone https://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@github.com/alexpicjava/Cifo-App-Web-Technologies/shell_Utils_Scripts_Suse.git /home/$USER/shell_Utils_Scripts_Suse
+git clone https://github.com/Cifo-App-Web-Technologies/shell_Utils_Scripts_Suse.git /home/$USER/shell_Utils_Scripts_Suse
+#git clone https://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@github.com/alexpicjava/Cifo-App-Web-Technologies/shell_Utils_Scripts_Suse.git /home/$USER/shell_Utils_Scripts_Suse
 
 #Update the Paths to the system be able to work whit the new folder.
 #PATH
