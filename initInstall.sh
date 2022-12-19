73b77de38f1ffe22f5c9e807cd2846da1a120bbe
alexpicjava <alex.pic.java@gmail.com>
2022-12-19 22:35:10 +0000

    [FIX]Refactoring initInstall.sh

--- initInstall.sh
+++ initInstall.sh
@@ -1,8 +1,10 @@
 #This is the initial steps in order to Install all programs and settings immediately after install a new OpenSuse Linux.
 #Be sure you work under root, but be careful.
 
+user=$USER
+
 #Switch to root
-#sudo -i
+sudo -i
 
 #Install git
 zypper install -y git
@@ -14,15 +16,15 @@ git config --global user.mail "alex.pic.java@gmail.com"
 #Create a local Repo and clone all necessary information to continue with the installation.
 # git clone https://oauth-key-goes-here@github.com/username/repo.git
 # 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
-git clone https://github.com/Cifo-App-Web-Technologies/shell_Utils_Scripts_Suse.git /home/$USER/shell_Utils_Scripts_Suse
+git clone https://github.com/Cifo-App-Web-Technologies/shell_Utils_Scripts_Suse.git /home/$user/shell_Utils_Scripts_Suse
 #git clone https://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@github.com/alexpicjava/Cifo-App-Web-Technologies/shell_Utils_Scripts_Suse.git /home/$USER/shell_Utils_Scripts_Suse
 
 #Update the Paths to the system be able to work whit the new folder.
 #PATH
-export PATH="$PATH:/home/$USER/shell_Utils_Scripts_Suse"
+export PATH="$PATH:/home/$user/shell_Utils_Scripts_Suse"
 
 #FPATH Fusntions Path
-export FPATH="$FPATH:/home/$USER/shell_Utils_Scripts_Suse/FuntionShell"
+export FPATH="$FPATH:/home/$user/shell_Utils_Scripts_Suse/FuntionShell"
 
 #Starting Installation
 installAll.sh
