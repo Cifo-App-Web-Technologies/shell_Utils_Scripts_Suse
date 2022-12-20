4d897bffff731eff834be4b58c87d781f9821c9f
alexpicjava <alex.pic.java@gmail.com>
2022-12-20 10:58:19 +0000

    [ADD] initGit.sh && [FIX] initInstall.sh

---
+++ initGit.sh
@@ -0,0 +1,25 @@
+#! bin/bash
+
+#Install git
+zypper install -y git
+
+#Config global info git
+git config --global user.name "alexpicjava"
+git config --global user.mail "alex.pic.java@gmail.com"
+
+#Create a local Repo and clone all necessary information to continue with the installation.
+# git clone https://oauth-key-goes-here@github.com/username/repo.git
+# key --> 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 --> Ended 01/12/22
+# git clone https://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@github.com/alexpicjava/Cifo-App-Web-Technologies/shell_Utils_Scripts_Suse.git /home/$USER/shell_Utils_Scripts_Suse
+git clone https://github.com/Cifo-App-Web-Technologies/shell_Utils_Scripts_Suse.git /home/$puser/shell_Utils_Scripts_Suse
+
+
+#Update the Paths to the system be able to work whit the new folder.
+#PATH
+#export PATH="$PATH:/home/$puser/shell_Utils_Scripts_Suse"
+
+#FPATH Fusntions Path
+#export FPATH="$FPATH:/home/$puser/shell_Utils_Scripts_Suse/FuntionShell"
+
+#Starting Installation
+installAll.sh
--- initInstall.sh
+++ initInstall.sh
@@ -1,23 +1,8 @@
+#! bin/bash
 #This is the initial steps in order to Install all programs and settings immediately after install a new OpenSuse Linux.
 
 export puser=$USER
 export inifol=$PWD
-
-#Switch to root
-sudo -E su root
-
-#Install git
-zypper install -y git
-
-#Config global info git
-git config --global user.name "alexpicjava"
-git config --global user.mail "alex.pic.java@gmail.com"
-
-#Create a local Repo and clone all necessary information to continue with the installation.
-# git clone https://oauth-key-goes-here@github.com/username/repo.git
-# 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 Ended 01/12/22
-git clone https://github.com/Cifo-App-Web-Technologies/shell_Utils_Scripts_Suse.git /home/$puser/shell_Utils_Scripts_Suse
-#git clone https://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@github.com/alexpicjava/Cifo-App-Web-Technologies/shell_Utils_Scripts_Suse.git /home/$USER/shell_Utils_Scripts_Suse
 
 #Update the Paths to the system be able to work whit the new folder.
 #PATH
@@ -26,6 +11,9 @@ export PATH="$PATH:/home/$puser/shell_Utils_Scripts_Suse"
 #FPATH Fusntions Path
 export FPATH="$FPATH:/home/$puser/shell_Utils_Scripts_Suse/FuntionShell"
 
+#Switch to root
+sudo -E su root && ./initGit.sh
+
 #Starting Installation
-installAll.sh
+#initGit.sh
 
